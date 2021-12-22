9980d78a3a856af2406787de7c9ba5e41461a40e
Dono Catur Prasetyo <donocaturprasetyo@gmail.com>
2021-12-22 15:13:26 +0700

    test case channels tabs edit

--- src/main/java/webautomation/mekarichat/utils/DataUtils.java
+++ src/main/java/webautomation/mekarichat/utils/DataUtils.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class DataUtils {
 	public static final String titlePageLogin = "Mekari Account";
-	public static final String emailMekari = "donocatur@qiscus.cx";
-	public static final String passwordMekari = "tanpapassword08";
+	public static final String emailMekari = "emailMekari";
+	public static final String passwordMekari = "passwordMekari";
 	public static final String urlDashboard = "https://messenger.mekari.com/dashboard";
 	public static final String fotoProfile = foto();
 	
